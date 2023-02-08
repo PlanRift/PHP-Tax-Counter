988119b0daed659d668c4498f9d44ba9f31baf94
PlanRift <dzikramufidatovid@gmail.com>
2023-02-08 09:03:36 +0700

    change req php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'planrift/pajakcounter',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '0535431fad350c9e969a5fa98f22fe2630ec3b87',
+        'reference' => 'e23b9aa6644b5693c1e48ea9ef4b8ea085504e3d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'planrift/pajakcounter' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '0535431fad350c9e969a5fa98f22fe2630ec3b87',
+            'reference' => 'e23b9aa6644b5693c1e48ea9ef4b8ea085504e3d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
